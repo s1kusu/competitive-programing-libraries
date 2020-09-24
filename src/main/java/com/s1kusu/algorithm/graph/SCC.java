9749eafb57e5d0f5f80b6a5fde20a5ae56b1c05d
s1kusu <s1kusunoki@gmail.com>
2020-09-24 23:13:41 +0900

    update SCC
    - add comment
    - rename scc() -> build()

--- src/main/java/com/s1kusu/algorithm/graph/SCC.java
+++ src/main/java/com/s1kusu/algorithm/graph/SCC.java
@@ -17,6 +17,11 @@ class SCC {
     private final int[] cmp;
     private final List<List<Integer>> sccList;
 
+    /**
+     * N頂点0辺の有向グラフを生成する.<br>
+     * 計算量：O(N)
+     * @param n
+     */
     @SuppressWarnings("unchecked")
     public SCC(int n) {
         this.n = n;
@@ -28,11 +33,54 @@ class SCC {
         this.sccList = new ArrayList<>(n);
     }
 
+    /**
+     * 有向辺を張る.<br>
+     * 計算量：O(1)
+     * @param from
+     * @param to
+     */
     public void addEdge(int from, int to) {
         if(G[from] == null) G[from] = new ArrayList<Integer>();
         G[from].add(to);
         if(rG[to] == null) rG[to] = new ArrayList<Integer>();
         rG[to].add(from);
+    }
+
+    /**
+     * 頂点kを含む競連結成分のトポロジカル順序を取得する.<br>
+     * 制約：先にbuild()を呼び出しておく必要がある.<br>
+     * 計算量：O(1)
+     * @param k
+     * @return 頂点kを含む競連結成分のトポロジカル順序
+     */
+    public int getOrder(int k) {
+        return cmp[k];
+    }
+
+    /**
+     * 強連結成分分解を行う.<br>
+     * 「強連結成分を構成する頂点のリスト」のリストを返却する.<br>
+     * 外側の強連結成分のリストはトポロジカルソートされる.<br>
+     * 内側の頂点のリスト内の順序は未定義.
+     * 計算量：O(N+M) (Mは追加した辺の数)
+     * @return
+     */
+    public List<List<Integer>> build(){
+        Arrays.fill(used, false);
+        rv.clear();
+        for (int i = 0; i < n; i++) {
+            if(used[i]) continue;
+            dfs(i);
+        }
+        Arrays.fill(used, false);
+        int k = 0;
+        for (int i = n-1; i >= 0; i--) {
+            if(used[rv.get(i)]) continue;
+            List<Integer> list = new ArrayList<>();
+            rdfs(rv.get(i), k++, list);
+            sccList.add(list);
+        }
+        return sccList;
     }
 
     private void dfs(int v) {
@@ -55,24 +103,6 @@ class SCC {
             if(used[nv]) continue;
             rdfs(nv, k, list);
         }
-    }
-
-    public List<List<Integer>> scc(){
-        Arrays.fill(used, false);
-        rv.clear();
-        for (int i = 0; i < n; i++) {
-            if(used[i]) continue;
-            dfs(i);
-        }
-        Arrays.fill(used, false);
-        int k = 0;
-        for (int i = n-1; i >= 0; i--) {
-            if(used[rv.get(i)]) continue;
-            List<Integer> list = new ArrayList<>();
-            rdfs(rv.get(i), k++, list);
-            sccList.add(list);
-        }
-        return sccList;
     }
 
 }
